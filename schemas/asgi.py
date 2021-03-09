3abe8b56d2e093bb877a00ea1c7d7c6b4fe24036
vitamal <vitamal@ukr.net>
2021-03-09 20:00:58 +0200

    #33 start to include channels

--- schemas/asgi.py
+++ schemas/asgi.py
@@ -9,8 +9,20 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
 from django.core.asgi import get_asgi_application
+
+import csv_generator.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schemas.project.settingsproxy')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            csv_generator.routing.websocket_urlpatterns
+        )
+    ),
+})
